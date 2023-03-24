e2e48b93bdb0e924b964c48bdcd34602280ee138
2 <748807622@qq.com>
2023-03-24 17:30:17 +0800

    test

--- party-server/src/main/java/com/im/imparty/ImPartyApplication.java
+++ party-server/src/main/java/com/im/imparty/ImPartyApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @MapperScan(value = {"com.im.imparty.**.mapper"})
 @SpringBootApplication
 public class ImPartyApplication {
