7cce4b036a0582e99e08572b80e21025f38d35d4
2 <748807622@qq.com>
2023-03-28 20:49:12 +0800

    登陆和缓存相关代码

---
+++ base-common/src/main/java/com/im/imparty/web/vo/BaseResult.java
@@ -0,0 +1,37 @@
+package com.im.imparty.web.vo;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+@Data
+public class BaseResult<T> {
+
+    public BaseResult(int code) {
+        this.code = code;
+    }
+
+    public BaseResult(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    private int code;
+
+    private String msg;
+
+    private T data;
+
+    public static <T> BaseResult<T> ok(String msg) {
+        return new BaseResult(200, msg);
+    }
+
+    public BaseResult<T> data(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public String toJSONString() {
+        return JSONObject.toJSONString(this);
+    }
+
+}
---
+++ party-server/src/main/java/com/im/imparty/config/security/LoginFailureHandler.java
@@ -0,0 +1,48 @@
+package com.im.imparty.config.security;
+
+import com.alibaba.fastjson.JSONObject;
+import com.im.imparty.web.vo.BaseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component("loginFailureHandler")
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws ServletException, IOException {
+
+        logger.info("登录失败");
+        response.setContentType("application/json;charset=UTF-8");
+        //这里写你登录失败后的逻辑，可加验证码验证等
+        String errorInfo = "";
+        if (exception instanceof BadCredentialsException ||
+                exception instanceof UsernameNotFoundException) {
+            errorInfo = "账户名或者密码输入错误!";
+        } else if (exception instanceof LockedException) {
+            errorInfo = "账户被锁定，请联系管理员!";
+        } else if (exception instanceof CredentialsExpiredException) {
+            errorInfo = "密码过期，请联系管理员!";
+        } else if (exception instanceof AccountExpiredException) {
+            errorInfo = "账户过期，请联系管理员!";
+        } else if (exception instanceof DisabledException) {
+            errorInfo = "账户被禁用，请联系管理员!";
+        } else {
+            errorInfo = "登录失败!";
+        }
+        logger.info("登录失败原因：" + errorInfo);
+        //ajax请求认证方式
+        response.getWriter().write(BaseResult.ok(errorInfo).data(exception).toJSONString());
+    }
+
+}
