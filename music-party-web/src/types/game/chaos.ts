d580780120122467a82a07fe4fda601c8b4706b6
Su <18640141630@163.com>
2023-04-16 17:31:53 +0800

    提交大乱斗前端代码

---
+++ music-party-web/src/types/game/chaos.ts
@@ -0,0 +1,44 @@
+export interface PersonFightInfo {
+    agi: number,
+    blk: number,
+    blkPer: number,
+    buff: Array<BuffInfo>,
+    ca: number,
+    cri: number,
+    criPer: number,
+    finalAtk: number,
+    finalDef: number,
+    hp: number,
+    htl: number,
+    name: string,
+    spd: number,
+    str: number,
+    usedWeapon: Array<string>,
+    wit: number,
+    wpInfo: WpInfo
+}
+
+export interface BuffInfo {
+    buff: string,
+    turns: number
+}
+
+export interface WpInfo {
+    skHolding: string,
+    userName: string,
+    wpHolding: string
+}
+
+
+export interface BattleInfo {
+    createTime: string,
+    defenseInfo: PersonFightInfo,
+    fightInfo: string,
+    id: string,
+    ifEnd: boolean,
+    message: string,
+    offensiveInfo: PersonFightInfo,
+    round: number,
+    surplusCd: string
+}
+
