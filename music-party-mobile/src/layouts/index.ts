4ff209119b786fc747203620db99f6154209c485
Su-wsm <18640141630@163.com>
2023-06-09 17:25:04 +0800

    提交

--- music-party-mobile/src/layouts/index.ts
+++ music-party-mobile/src/layouts/index.ts
@@ -3,6 +3,6 @@ const BlankLayout = () => import('./BlankLayout/index.vue');
 const AuthLayout = () => import('./AuthLayout.vue');
 const ErrorLayout = () => import('./ErrorLayout.vue');
 const TodoLayout= () => import('../views/todo/TodoLayout.vue');
-const ChatLayout= () => import('../views/chart/ChatPage.vue');
+const ChatLayout= () => import('../views/chat/ChatPage.vue');
 
 export {BasicLayout, BlankLayout, AuthLayout, ErrorLayout, TodoLayout , ChatLayout};
