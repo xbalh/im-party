88152e12f84887002e87ec633c0939a7bfc63670
Su-wsm <18640141630@163.com>
2023-07-05 17:28:15 +0800

    按钮位置同步

--- music-party-mobile/src/components/common/RightCircleButton.vue
+++ music-party-mobile/src/components/common/RightCircleButton.vue
@@ -3,31 +3,33 @@
     <!-- 聊天室 -->
     <div>
       <!-- 隐藏到右侧 -->
-      <v-btn theme="dark" ref="refButton1" icon="mdi-forum-outline" class="drawer-button"
-        color="#55BB46" v-if="isMusicPage && dragDotRight" @click="dragDotRight = false">
+      <v-btn theme="dark" ref="chatBtnDot" icon="mdi-forum-outline" class="drawer-button" color="#55BB46"
+        v-show="isMusicPage && dragDotRight" @click="dragDotRight = false">
         <v-badge dot offset-x="15" offset-y="-10" color="red">
           <v-icon class="fa-spin">mdi-forum-outline</v-icon>
         </v-badge>
       </v-btn>
       <!-- 完全显示 -->
-      <v-btn theme="dark" ref="refButton2" icon="mdi-forum-outline" class="drawer-button1 "
-        color="#55BB46" v-if="isMusicPage && !dragDotRight" v-dialogDrag::click="toChatPage">
-        <v-badge content="99+" offset-x="-10" offset-y="-10" color="red">
-          <v-icon class="fa-spin">mdi-forum-outline</v-icon>
-        </v-badge>
-      </v-btn>
+      <div class="drawer-button1" ref="chatBtn" v-dialogDrag="true" v-dialogDrag::click="toChatPage">
+        <v-btn theme="dark" icon="mdi-forum-outline" color="#55BB46"
+          v-show="isMusicPage && !dragDotRight" >
+          <v-badge content="99+" offset-x="-10" offset-y="-10" color="red">
+            <v-icon class="fa-spin">mdi-forum-outline</v-icon>
+          </v-badge>
+        </v-btn>
+      </div>
     </div>
     <!-- 音乐播放 -->
     <div>
       <!-- 隐藏到右侧 -->
-      <v-btn theme="dark" ref="refButton1" icon="mdi-music" class="music-button" color="#ec4444"
+      <v-btn theme="dark" ref="musicBtnDot" icon="mdi-music" class="music-button" color="#ec4444"
         v-if="!isMusicPage && dragDotRight" @click="dragDotRight = false">
         <v-icon class="fa-spin">mdi-music</v-icon>
       </v-btn>
       <!-- 完全显示 -->
-      <div class="music-button1" v-dialogDrag::click="toMusicPage">
-        <v-progress-circular v-model="progress" class="me-2" size="60" width="3" v-if="!isMusicPage && !dragDotRight">
-          <v-btn theme="dark" ref="refButton2" icon="mdi-music" color="#ec4444" v-if="!dragDotRight">
+      <div class="music-button1" ref="musicBtn" v-dialogDrag="true" v-dialogDrag::click="toMusicPage">
+        <v-progress-circular v-model="progress" class="me-2" size="60" width="3" v-show="!isMusicPage && !dragDotRight">
+          <v-btn theme="dark" ref="refButton2" icon="mdi-music" color="#ec4444" v-show="!dragDotRight">
             <v-icon class="fa-spin">mdi-music</v-icon>
           </v-btn>
         </v-progress-circular>
@@ -39,9 +41,31 @@
 
 <script setup lang="ts">
 import Bus from "@/utils/common/Bus";
+
+const chatBtn = ref<HTMLElement>()
+
+const musicBtn = ref<HTMLElement>()
+
 const isMusicPage = ref(false)
 
-const refButton = ref<import('vuetify/components').VBtn>()
+watch(isMusicPage, (newValue, oldValue) => {
+  if (newValue) {
+    //为true：音乐按钮隐藏，聊天按钮展示，把音乐按钮位置赋给聊天按钮
+    const top = musicBtn.value!.style.top
+    const left = musicBtn.value!.style.left
+    chatBtn.value!.style.cssText += `;left:${left};top:${top};`
+  } else {
+    //为false：聊天按钮隐藏，音乐按钮展示，把聊天按钮位置赋给音乐按钮
+    const top = chatBtn.value!.style.top
+    const left = chatBtn.value!.style.left
+    musicBtn.value!.style.cssText += `;left:${left};top:${top};`
+
+  }
+
+  // console.log('值发生了变更', newValue, oldValue);
+});
+
+
 
 const dragDotRight = ref(false)
 Bus.on('dragDotRight', (flag: boolean) => {
@@ -101,12 +125,16 @@ onMounted(() => {
 }
 
 .drawer-button1 {
+  width: 60px;
   position: fixed;
   top: 340px;
   right: 35px;
   z-index: 9999;
-  box-shadow: 1px 1px 18px #55BB46;
   // opacity: 75%;
+
+  .v-btn {
+    box-shadow: 1px 1px 18px #55BB46;
+  }
 
   .v-icon {
     // margin-left: -18px;
