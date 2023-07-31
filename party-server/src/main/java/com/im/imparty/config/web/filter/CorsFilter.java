bcffd93f743c920b474a37e04da39ccf1b6f4ddc
Su-wsm <18640141630@163.com>
2023-07-31 15:31:28 +0800

    试一下解决跨域问题

--- party-server/src/main/java/com/im/imparty/config/web/filter/CorsFilter.java
+++ party-server/src/main/java/com/im/imparty/config/web/filter/CorsFilter.java
@@ -26,7 +26,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "trm_access_token,X-Requested-With,Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "trm_access_token,X-Requested-With,Content-Type,Authorization");
         filterChain.doFilter(request, response);
     }
 
