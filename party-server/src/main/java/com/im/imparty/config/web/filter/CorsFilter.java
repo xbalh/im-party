652ac81ec5e48280d94edfc588cda472211beaea
Su-wsm <18640141630@163.com>
2023-10-19 16:35:51 +0800

    还原跨域配置

--- party-server/src/main/java/com/im/imparty/config/web/filter/CorsFilter.java
+++ party-server/src/main/java/com/im/imparty/config/web/filter/CorsFilter.java
@@ -22,11 +22,11 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers", "trm_access_token,X-Requested-With,Content-Type,Authorization");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "trm_access_token,X-Requested-With,Content-Type,Authorization");
         filterChain.doFilter(request, response);
     }
 
